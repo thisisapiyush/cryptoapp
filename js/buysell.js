03faed44b323708275bb6474441ab6843cbe7ba1
thisisapiyush <47004090+thisisapiyush@users.noreply.github.com>
2021-10-19 21:17:23 +0930

    JS file for the Buy/Sell/Swap page

---
+++ js/buysell.js
@@ -0,0 +1,13 @@
+function myFunction() {
+  alert("You bought.");
+}
+function myFunction1() {
+    alert("You made a deposit.");
+}
+function myFunction2() {
+  alert("You sold.");
+}               
+
+function myFunction4() {
+  alert("You swapped.");
+}                             
